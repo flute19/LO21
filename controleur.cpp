8bbb5e87e050ce30330c3b6bfadacca6700bb7a9
flute19 <alexia.lequilliec@orange.fr>
2016-06-11 22:54:11 +0200

    stop dev, test expression

--- controleur.cpp
+++ controleur.cpp
@@ -51,41 +51,29 @@ void Controleur::commande(const QString& c){
 
                                 Litterale& e = litMng.addLitterale(res.toString());
                                 litAff.push(e);
-                            }
-
-                                                            //                        }
-//                        if (!res){ litAff.setMessage("L'opération a echouée");}
-//                        Litterale& ref = *res;
-                        //Litterale& e = litMng.addLitterale(ref.toString());
-                        //litAff.push(e);
+                            }                                        
             } else{
                         litAff.setMessage("Erreur : pas assez d'arguments");
                     }
                 break;
-//                case 1:if (litAff.taille()>=1) {
-                    //                Litterale v2=litAff.top().toString();
-                    //                litMng.removeLitterale(litAff.top());
-                    //                litAff.pop();
-                    //                Litterale v1=litAff.top().toString();
-                    //                litMng.removeLitterale(litAff.top());
-                    //                litAff.pop();
-                    //                int res;
-                    //                if (c=="+") res=v1+v2;
-                    //                if (c=="-") res=v1-v2;
-                    //                if (c=="*") res=v1*v2;
-                    //                if (c=="/") {
-                    //                    if (v2!=0) res=v1/v2;
-                    //                    else {
-                    //                        litAff.setMessage("Erreur : division par zéro");
-                    //                        res=v1;
-                    //                    }
-                    //                }
-                    //                Litterale& e=litMng.addLitterale(res);
-                    //                litAff.push(e);
-//                                }else{
-//                                    litAff.setMessage("Erreur : pas assez d'arguments");
-//                                }
-//                break;
+                case 1:if (litAff.taille()>=1) {
+
+                    Litterale& v1 = litAff.top();
+                    litMng.removeLitterale(litAff.top());
+                    litAff.pop();
+                    LitteraleCalculable& val1 = dynamic_cast<LitteraleCalculable&>(v1);
+
+                                    if (c=="NEG"){
+
+                                        LitteraleCalculable& res = NEG(val1);
+                                        Litterale& e = litMng.addLitterale(res.toString());
+                                        litAff.push(e);
+                                    }
+
+                                }else{
+                                    litAff.setMessage("Erreur : pas assez d'arguments");
+                                }
+                break;
             }
         }else litAff.setMessage("Erreur : commande inconnue");
 
@@ -97,6 +85,9 @@ int getArite(QString c){
     if (c == "+" || c == "-" || c == "*" || c == "/"){
         return 2;
     }
+    if (c == "NEG"){
+        return 1;
+    }
     return 0;
 }
 
@@ -105,6 +96,8 @@ bool estUnOperateur(const QString s){
     if (s=="-") return true;
     if (s=="*") return true;
     if (s=="/") return true;
+    if (s=="NEG") return true;
+
     return false;
 }
 
@@ -113,7 +106,7 @@ int estUnNombre(const QString s){
   // QRegExp rx("[/]"); //rationnel
 
 
-   if(s.toInt(&ok)) return 0;
+   if(s.toInt(&ok) || s == "0") return 0;
    if(s.toFloat(&ok)) return 1;
   // if(s.contains(&rx)) return 2;
 
